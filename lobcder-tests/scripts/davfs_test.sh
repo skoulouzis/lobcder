a8874214e044abe438c4f8859dea8ff46e067fa0
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-05-04 18:31:56 +0000

    Script

--- lobcder-tests/scripts/davfs_test.sh
+++ lobcder-tests/scripts/davfs_test.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-ls .
+
+
+LOBCDER_URL=http://localhost:8080/lobcder-1.0-SNAPSHOT/
+MOUNT_POINT=/media/LOBCDER-LOCAL
+USER_NAME=$USER
+
+
+sudo mkdir /media/LOBCDER
+sudo mount -t davfs  $LOBCDER_URL $MOUNT_POINT -o rw,uid=$USER_NAME
+
+
+#HOST1
+#Generate files in $HOME 
+#Copy (upload) them in $MOUNT_POINT
+
+#HOST2 
+#Process data in $MOUNT_POINT
+#Delete 
+
+#HOST1
+#?
