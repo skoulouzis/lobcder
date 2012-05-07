d0739c87be4be048a975b259a7ab90ee0a1ba244
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-05-07 12:54:08 +0000

    Script

--- lobcder-tests/scripts/davfs_test.sh
+++ lobcder-tests/scripts/davfs_test.sh
@@ -2,12 +2,44 @@
 
 
 LOBCDER_URL=http://localhost:8080/lobcder-1.0-SNAPSHOT/
-MOUNT_POINT=/media/LOBCDER-LOCAL
-USER_NAME=$USER
+MOUNT_POINT=/media/LOBCDER-TEST
 
 
-sudo mkdir /media/LOBCDER
-sudo mount -t davfs  $LOBCDER_URL $MOUNT_POINT -o rw,uid=$USER_NAME
+
+USER_NAME1=$USER
+HOST1=localhost
+PASS1=secret
+
+#Make mount point 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS1 | sudo -S mkdir $MOUNT_POINT"
+
+#Make davfs configuration dir 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "mkdir ~/.davfs2/" 
+#Copy davfs conf file and secrets file 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS | sudo -S cp /etc/davfs2/davfs2.conf /etc/davfs2/secrets ~/.davfs2/" 
+
+#Change permissions 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS | sudo -S sudo chmod 666 ~/.davfs2/secrets" 
+
+#Add the password
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS | sudo -S echo "$MOUNT_POINT $DAV_USER $DAV_PASS">> ~/.davfs2/secrets "
+~/.davfs2/secrets
+
+#Change permissions 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS | sudo -S sudo chmod 600 ~/.davfs2/secrets" 
+
+
+#Mount it 
+sshpass -p "$PASS1" ssh $USER_NAME1@$HOST1 "echo $PASS1 | sudo -S mount -t davfs  $LOBCDER_URL $MOUNT_POINT -o rw,uid=$USER_NAME"
+
+#USER_NAME1=$USER
+#HOST2=host.2.com
+
+
+
+
+
+
 
 
 #HOST1
