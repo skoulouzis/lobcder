d031e30431b3dbe25608ca5d03419c6deebcfc17
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-05-23 17:01:10 +0000

    Scale test

--- lobcder-tests/src/test/java/nl/uva/cs/lobcder/tests/LobcderScalabilityTest.java
+++ lobcder-tests/src/test/java/nl/uva/cs/lobcder/tests/LobcderScalabilityTest.java
@@ -9,6 +9,12 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
@@ -18,6 +24,7 @@ import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.commons.httpclient.protocol.Protocol;
 import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+import org.apache.cxf.helpers.FileUtils;
 import org.apache.jackrabbit.webdav.client.methods.DeleteMethod;
 import org.apache.jackrabbit.webdav.client.methods.MkColMethod;
 import org.apache.jackrabbit.webdav.client.methods.PutMethod;
@@ -32,11 +39,13 @@ import org.junit.Test;
 public class LobcderScalabilityTest {
 
     private URI uri;
-    private String root;
+    private String lobcderRoot;
     private String password;
     private String[] usernames;
     private HttpClient[] clients;
     private ArrayList<File> datasets;
+    public static final int TEST_UPLOAD = 0;
+    public static final int CREATE_DATASET = 1;
 
     @Before
     public void setUp() throws Exception {
@@ -66,9 +75,9 @@ public class LobcderScalabilityTest {
         }
 
         this.uri = URI.create(testURL);
-        this.root = this.uri.toASCIIString();
-        if (!this.root.endsWith("/")) {
-            this.root += "/";
+        this.lobcderRoot = this.uri.toASCIIString();
+        if (!this.lobcderRoot.endsWith("/")) {
+            this.lobcderRoot += "/";
         }
     }
 
@@ -99,9 +108,9 @@ public class LobcderScalabilityTest {
         }
     }
 
-    private void initDatasets() throws IOException {
+    private void initDatasets() throws IOException, InterruptedException {
         //Init a synthetic datasets from 50MB to 4GB
-        int start = 14;
+        int start = 20;
         int end = 100;
         String dirPath = System.getProperty("java.io.tmpdir") + "/testDatasets";
         File dataSetFolderBase = new File(dirPath);
@@ -110,16 +119,41 @@ public class LobcderScalabilityTest {
                 throw new IOException("Faild to create tmp dir");
             }
         }
-        byte[] data = new byte[1024 * 100];//1MB
-        Random r = new Random();
-        
-        String datasetName;
-        String datasetPath;
+
         datasets = new ArrayList<File>();
-        FileOutputStream fos;
-        for (int i = start; i < end; i *= 2) {
-            datasetName = "dataset" + i + "MB";
-            datasetPath = dataSetFolderBase.getAbsolutePath() + "/" + datasetName;
+        createFiles(32, start, end, dataSetFolderBase);
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException, IOException, InterruptedException {
+        try {
+            //Create the folder stucture 
+            File dataset = datasets.get(0);
+            String[] parts = dataset.getAbsolutePath().split("/");
+            String testDatasetPath = this.lobcderRoot;
+            for (int i = 1; i < parts.length - 1; i++) {
+                if (testDatasetPath.endsWith("/")) {
+                    testDatasetPath += parts[i];
+                } else {
+                    testDatasetPath += "/" + parts[i];
+                }
+                System.out.println("MkCol: " + testDatasetPath);
+                MkColMethod mkcol = new MkColMethod(testDatasetPath);
+                clients[0].executeMethod(mkcol);
+                assertTrue("status: " + mkcol.getStatusCode(), mkcol.getStatusCode() == HttpStatus.SC_CREATED || mkcol.getStatusCode() == HttpStatus.SC_METHOD_NOT_ALLOWED);
+            }
+            
+            upload(testDatasetPath, 5);
+        } finally {
+        }
+    }
+
+    private void createFiles(int threads, int start, int end, File dataSetFolderBase) throws IOException, InterruptedException {
+        ExecutorService execSvc = Executors.newFixedThreadPool(threads);
+        long startTime = System.currentTimeMillis();
+        for (int i = start; i < end; i *= 4) {
+            String datasetName = "dataset" + i + "MB";
+            String datasetPath = dataSetFolderBase.getAbsolutePath() + "/" + datasetName;
             File dataset = new File(datasetPath);
             if (!dataset.exists()) {
                 if (!dataset.mkdirs()) {
@@ -128,54 +162,162 @@ public class LobcderScalabilityTest {
             }
             datasets.add(dataset);
             for (int j = 0; j < i; j++) {
-                File f = new File(dataset.getAbsolutePath() + "/file" + j);
-                if (!f.exists()) {
-                    fos = new FileOutputStream(f);
-                    r.nextBytes(data);
-                    fos.write(data);
-                    fos.flush();
-                    fos.close();
-                }
+                ScaleTest u = new ScaleTest(CREATE_DATASET);
+                u.setDataset(dataset);
+                u.setFileID(j);
+                execSvc.execute(u);
             }
+        }
+        execSvc.shutdown();
+        execSvc.awaitTermination(30, TimeUnit.MINUTES);
+        long endTime = System.currentTimeMillis();
+        System.out.println(threads + " , " + (endTime - startTime));
+    }
+
+    void delete(File f) throws IOException {
+        if (f.isDirectory()) {
+            for (File c : f.listFiles()) {
+                delete(c);
+            }
+        }
+        if (!f.delete()) {
+            throw new FileNotFoundException("Failed to delete file: " + f);
         }
     }
 
-    @Test
-    public void testUpload() throws FileNotFoundException, IOException {
-        try {
-            File dataset = datasets.get(0);
-            String[] parts = dataset.getAbsolutePath().split("/");
-            String testDatasetPath = this.root;
-            for (int i = 1; i < parts.length; i++) {
-                if (testDatasetPath.endsWith("/")) {
-                    testDatasetPath += parts[i];
-                } else {
-                    testDatasetPath += "/" + parts[i];
+    private void upload(String testDatasetPath, int threads) throws IOException, InterruptedException {
+
+        ExecutorService execSvc = Executors.newFixedThreadPool(threads);
+        for (int i = 0; i < threads; i++) {
+            ScaleTest user = new ScaleTest(TEST_UPLOAD);
+            user.setClient(clients[i]);
+            user.setDataset(datasets);
+            user.setLobcderURL(lobcderRoot);
+            user.setWorkingPath(testDatasetPath);
+            execSvc.execute(user);
+        }
+        execSvc.shutdown();
+        execSvc.awaitTermination(30, TimeUnit.MINUTES);
+    }
+
+    private static class ScaleTest implements Runnable {
+
+        private ArrayList<File> datasets;
+        private String lobcderRoot;
+        private HttpClient client;
+        private String username;
+        private String testDatasetPath;
+        private final int op;
+        private File dataset;
+        private int fileID;
+
+        private ScaleTest(int op) {
+            this.op = op;
+        }
+
+        @Override
+        public void run() {
+            try {
+                switch (op) {
+                    case TEST_UPLOAD:
+                        initWorkingURL();
+                        upload();
+                        break;
+                    case CREATE_DATASET:
+                        createDataset(10);
+                    default:
+                        break;
                 }
-                System.out.println("----> " + testDatasetPath);
-                MkColMethod mkcol = new MkColMethod(testDatasetPath);
-                clients[0].executeMethod(mkcol);
+
+            } catch (Exception ex) {
+                Logger.getLogger(LobcderScalabilityTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        private void upload() throws IOException {
+            for (File d : datasets) {
+                File[] files = d.listFiles();
+                String parent = files[0].getParentFile().getName();
+                String path1 = testDatasetPath + "/" + parent;
+                debug("Mkol: " + path1);
+                MkColMethod mkcol = new MkColMethod(path1);
+                client.executeMethod(mkcol);
                 assertTrue("status: " + mkcol.getStatusCode(), mkcol.getStatusCode() == HttpStatus.SC_CREATED || mkcol.getStatusCode() == HttpStatus.SC_METHOD_NOT_ALLOWED);
+                for (File f : files) {
+                    String path2 = path1 + "/" + f.getName();
+                    debug("PUT: " + path2);
+                    PutMethod put = new PutMethod(path2);
+                    RequestEntity requestEntity = new InputStreamRequestEntity(
+                            new FileInputStream(f));
+                    put.setRequestEntity(requestEntity);
+                    client.executeMethod(put);
+                    assertEquals(HttpStatus.SC_CREATED, put.getStatusCode());
+                }
             }
 
-            File[] files = dataset.listFiles();
-            for (File f : files) {
-                String path = testDatasetPath + "/" + f.getName();
-                System.out.println("PUT: " + path);
-                PutMethod put = new PutMethod(path);
-                RequestEntity requestEntity = new InputStreamRequestEntity(
-                        new FileInputStream(f));
-                put.setRequestEntity(requestEntity);
-                clients[0].executeMethod(put);
-                assertEquals(HttpStatus.SC_CREATED, put.getStatusCode());
-                
+//            File[] files = dataset.listFiles();
+//            for (File f : files) {
+//                String path = testDatasetPath + "/" + f.getName();
+//                debug("PUT: " + path);
+//                PutMethod put = new PutMethod(path);
+//                RequestEntity requestEntity = new InputStreamRequestEntity(
+//                        new FileInputStream(f));
+//                put.setRequestEntity(requestEntity);
+//                client.executeMethod(put);
+//                assertEquals(HttpStatus.SC_CREATED, put.getStatusCode());
+//            }
+        }
+
+        private void setDataset(ArrayList<File> datasets) {
+            this.datasets = datasets;
+        }
+
+        private void setLobcderURL(String lobcderRoot) {
+            this.lobcderRoot = lobcderRoot;
+        }
+
+        private void debug(String msg) {
+            System.err.println(this.getClass().getSimpleName() + "." + username + ":" + msg);
+        }
+
+        private void setWorkingPath(String testDatasetPath) {
+            this.testDatasetPath = testDatasetPath;
+        }
+
+        private void initWorkingURL() throws IOException {
+            testDatasetPath += "/" + username;
+            MkColMethod mkcol = new MkColMethod(testDatasetPath);
+            client.executeMethod(mkcol);
+            assertTrue("status: " + mkcol.getStatusCode(), mkcol.getStatusCode() == HttpStatus.SC_CREATED || mkcol.getStatusCode() == HttpStatus.SC_METHOD_NOT_ALLOWED);
+        }
+
+        private void setClient(HttpClient httpClient) {
+            this.client = httpClient;
+
+            Credentials cred = client.getState().getCredentials(AuthScope.ANY);
+            this.username = cred.toString().split(":")[0];
+        }
+
+        private void createDataset(int sizeInk) throws FileNotFoundException, IOException {
+            byte[] data = new byte[1024 * sizeInk];//1MB
+            Random r = new Random();
+            File f = new File(dataset.getAbsolutePath() + "/file" + this.fileID);
+            if (!f.exists()) {
+//                debug("Writing: " + f.getAbsolutePath());
+                FileOutputStream fos = new FileOutputStream(f);
+                r.nextBytes(data);
+                fos.write(data);
+                fos.flush();
+                fos.close();
             }
+        }
 
-            DeleteMethod delete = new DeleteMethod(testDatasetPath+"/");
-            clients[0].executeMethod(delete);
-            System.out.println(delete.getStatusCode() + " " + delete.getStatusText());
+        private void setDataset(File dataset) {
+            this.dataset = dataset;
+        }
 
-        } finally {
+        private void setFileID(int j) {
+            this.fileID = j;
         }
     }
 }
