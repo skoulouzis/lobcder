cbaf68b5f2a97b25b83d1c6ae8a076c813a8412c
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-07-12 23:03:28 +0000

    Change test range

--- lobcder-tests/src/test/java/nl/uva/cs/lobcder/tests/LobcderScalabilityTest.java
+++ lobcder-tests/src/test/java/nl/uva/cs/lobcder/tests/LobcderScalabilityTest.java
@@ -51,11 +51,11 @@ public class LobcderScalabilityTest {
     public static final int TEST_DOWNLOAD = 2;
     public static final int DELETE = 3;
     
-    public static final int FILE_SIZE_IN_KB = 3200;
+    public static final int FILE_SIZE_IN_KB = 100;
     
     public static final int STEP_SIZE_DATASET = 4;
-    public static final int MIN_SIZE_DATASET = 40;
-    public static final int MAX_SIZE_DATASET = 1200;
+    public static final int MIN_SIZE_DATASET = 10;//640;
+    public static final int MAX_SIZE_DATASET = 640;//1200;
     public static final int NUM_OF_CLIENTS = 1;
     public static final String[] meanLables = new String[]{ScaleTest.userMeasureLablesPut[0], ScaleTest.userMeasureLablesPut[1], ScaleTest.userMeasureLablesPut[2], ScaleTest.userMeasureLablesPut[3], ScaleTest.userMeasureLablesPut[4], ScaleTest.userMeasureLablesPut[5], "NumOfUsers"};
     public static String measuresPath = "measures";
