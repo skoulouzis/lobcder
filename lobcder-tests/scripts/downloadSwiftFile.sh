56572e2bb1852f065efeea46760d25ca146e2df4
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-07-23 12:04:47 +0000

    Deleting project dir

--- lobcder-tests/scripts/downloadSwiftFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-        python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U user -K key download LOBCDER-REPLICA-vTEST
-
-#for f in $(ls $1)
-#do
-#	blobName=$(echo "$1/$f" | awk '{print substr($1,2)}')
-#	python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U user -K key download LOBCDER-REPLICA-vTEST $blobName  
-#done
