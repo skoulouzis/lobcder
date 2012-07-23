78e6de9851bfd0d70057a3bbf81870b6fd463141
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-07-23 11:43:13 +0000

    Scripts

--- lobcder-tests/scripts/downloadSwiftFile.sh
+++ lobcder-tests/scripts/downloadSwiftFile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-        python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U vphdemo:vphdemo -K LibiDibi7 download LOBCDER-REPLICA-vTEST
+        python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U user -K key download LOBCDER-REPLICA-vTEST
 
 #for f in $(ls $1)
 #do
 #	blobName=$(echo "$1/$f" | awk '{print substr($1,2)}')
-#	python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U vphdemo:vphdemo -K LibiDibi7 download LOBCDER-REPLICA-vTEST $blobName  
+#	python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U user -K key download LOBCDER-REPLICA-vTEST $blobName  
 #done
--- lobcder-tests/scripts/uploadSwiftFile.sh
+++ lobcder-tests/scripts/uploadSwiftFile.sh
@@ -4,7 +4,7 @@ echo $1
 for f in $(ls $1)
 do
 
-	python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U vphdemo:vphdemo -K LibiDibi7 upload LOBCDER-REPLICA-vTEST $1/$f 
+	python2.6  /home/skoulouz/Documents/scripts/swift -A https://149.156.10.131:8443/auth/v1.0 -U user -K key upload LOBCDER-REPLICA-vTEST $1/$f 
 #  cat $1/$f
   
 done
