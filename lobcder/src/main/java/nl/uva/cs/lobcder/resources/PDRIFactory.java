968c000e54f880f05e68e794dfbb466abbd73b63
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-10-16 18:48:45 +0000

    Fixed conflicts

--- lobcder/src/main/java/nl/uva/cs/lobcder/resources/PDRIFactory.java
+++ lobcder/src/main/java/nl/uva/cs/lobcder/resources/PDRIFactory.java
@@ -11,8 +11,9 @@ import java.io.IOException;
  * @author dvasunin
  */
 public class PDRIFactory {
+
     static private PDRIFactory factory = new PDRIFactory();
-    
+
     static public PDRIFactory getFactory() {
         return factory;
     }
@@ -20,8 +21,8 @@ public class PDRIFactory {
     public PDRI createInstance(String url, Long storageSiteId, String resourceUrl, String username, String password) throws IOException {
         if(resourceUrl.startsWith("file://")) {
             return new SimplePDRI(storageSiteId, url);
-        } else 
+        } else {
             return new VPDRI(url, storageSiteId, resourceUrl, username, password);
+        }
     }
-    
 }
