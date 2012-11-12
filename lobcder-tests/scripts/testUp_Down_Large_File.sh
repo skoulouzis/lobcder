a238f7f7876b6534ef8cff0f90d703feb19b8882
S. Koulouzis <sS.Koulouzis@uva.nl>
2012-11-12 19:42:00 +0000

    test script

--- lobcder-tests/scripts/testUp_Down_Large_File.sh
+++ lobcder-tests/scripts/testUp_Down_Large_File.sh
@@ -5,82 +5,158 @@
 # sleep 5
 
 
-BASE_DIR=$HOME/workspace/lobcder-tests
+function initVariables {
+        BASE_DIR=$HOME/workspace/lobcder-tests
+        HOST_NAME="elab.lab.uvalight.net"
+        SERVER_PATH="/tomcatWebDAV" #"/lobcder-2.0-SNAPSHOT/dav"
 
-WEB_DAV_HOST="elab.lab.uvalight.net"
-WEB_DAV_PATH="/tomcatWebDAV" #"/lobcder-2.0-SNAPSHOT/dav"
-if [ "$1" = "lob" ];
-then
-	WEB_DAV_PATH="/lobcder-2.0-SNAPSHOT/dav"
-fi
+        PORT="8083"
+        URL="http://$HOST_NAME:$PORT$SERVER_PATH"
 
-if [ "$1" = "dav" ];
-then
-        WEB_DAV_PATH="/tomcatWebDAV"
-fi
+        TEST_FILE_NAME=testLargeUpload
+        TEST_FILE_SERVER_PATH=$HOME/tmp/$TEST_FILE_NAME
+        let TEST_FILE_SIZE=1024*1024*1000
 
-WEB_DAV_PORT="8083"
-WEB_DAV_URL="http://$WEB_DAV_HOST:$WEB_DAV_PORT$WEB_DAV_PATH"
 
-TEST_FILE_NAME=testLargeUpload
-TEST_FILE_PATH=$HOME/tmp/$TEST_FILE_NAME
-let TEST_FILE_SIZE=1024*1024*1000
+        if [ "$1" = "lob" ];
+        then
+                SERVER_PATH="/lobcder-2.0-SNAPSHOT/dav"
+        fi
 
+        if [ "$1" = "dav" ];
+        then
+                SERVER_PATH="/tomcatWebDAV"
+        fi
+        if [ "$1" = "ftp" ];
+        then
+                SERVER_PATH="/ftp"
+                PORT=21
+                URL="ftp://$HOST_NAME$SERVER_PATH"
+        fi
+        echo "BASE_DIR $BASE_DIR"
+        echo "HOST_NAME $HOST_NAME"
+        echo "URL $URL"
+}
+
+function initMeasurePathAndFile {
 # --------------------Check dir and test files-------------------
-#WEB_DAV_MOUNT_POINT=`mount | grep elab | awk '{print $3;}'`
-#if [ -z $WEB_DAV_MOUNT_POINT ]; 
-#then
-#	echo "webdav is no mounted"
-#	exit -1
-#fi
+        if [ ! -d "$BASE_DIR/measures/$HOST_NAME/$SERVER_PATH" ];
+        then
+                mkdir -p $BASE_DIR/measures/$HOST_NAME/$SERVER_PATH
+        fi
 
-if [ ! -d "$BASE_DIR/measures/$WEB_DAV_HOST/$WEB_DAV_PATH" ]; 
-then
-	mkdir -p $BASE_DIR/measures/$WEB_DAV_HOST/$WEB_DAV_PATH
-fi
+        if [ ! -f  $TEST_FILE_SERVER_PATH ];
+        then
+                echo "File $TEST_FILE_SERVER_PATH does not exist."
+                dd if=/dev/zero of=$TEST_FILE_SERVER_PATH bs=$TEST_FILE_SIZE count=1
+        fi
 
-if [ ! -f  $TEST_FILE_PATH ];
-then
-	echo "File $TEST_FILE_PATH does not exist."
-	dd if=/dev/zero of=$TEST_FILE_PATH bs=$TEST_FILE_SIZE count=1
-fi
+        echo "SERVER_PATH $BASE_DIR/measures/$HOST_NAME/$SERVER_PATH"
+}
 
 
-# ---------------------Add header -----------------------------
-BWM_FILE_PATH=$BASE_DIR/measures/$WEB_DAV_HOST/$WEB_DAV_PATH/bwm-up.csv
-echo "unix_timestamp;iface_name;bytes_out;bytes_in;bytes_total;packets_out;packets_in;packets_total;errors_out;errors_in" > $BWM_FILE_PATH
+function initMeasureFileAndCadaverScript {
+#	echo "ARGS: 1: $1 2: $2 3: $3 4 $4"
+	rm -f $HOME/.netrc
+        echo "machine $HOST_NAME" > $HOME/.netrc
+        echo "	login $3" >> $HOME/.netrc
+        echo "	password $4" >> $HOME/.netrc
+
+        echo open $URL > cadaver.script
+
+        if [ "$2" = "ftp" ];
+        then
+                #echo open $HOST_NAME > cadaver.script
+		rm -f $HOME/.netrc
+        fi
+
+
+        if [ "$1" = "up" ];
+        then
+                echo put $TEST_FILE_SERVER_PATH >> cadaver.script
+                BWM_FILE_SERVER_PATH=$BASE_DIR/measures/$HOST_NAME/$SERVER_PATH/bwm-up.csv
+                echo "UP!"
+        fi
+
+        if [ "$1" = "down" ];
+        then
+                echo get $TEST_FILE_NAME >> cadaver.script
+                BWM_FILE_SERVER_PATH=$BASE_DIR/measures/$HOST_NAME/$SERVER_PATH/bwm-down.csv
+                echo "DOWN!"
+        fi
+        echo quit >> cadaver.script
+        echo "BWM_FILE $BWM_FILE_SERVER_PATH"
+}
+
+
+function start {
+        CMD="cadaver < cadaver.script"
+        if [ "$1" = "ftp" ];
+        then
+		CMD="ftp << cadaver.script"
+	fi
+
+echo "Command $CMD"
+echo "bwm-ng path $BWM_FILE_SERVER_PATH"
 # ---------------------Start monitoring-----------------------------
-bwm-ng -o csv -I lo -T rate -t 100 >> $BWM_FILE_PATH &
-BWM_PID=$!
-sleep 1
-#----------------------Make cadaver script-----------------------------------
-echo open $WEB_DAV_URL > cadaver.script
-echo put $TEST_FILE_PATH >> cadaver.script
-echo exit >> cadaver.script
+bwm-ng -o csv -I lo -T rate -t 100 >> $BWM_FILE_SERVER_PATH &
+       BWM_PID=$!
+       sleep 1
 #----------------------start copy-----------------------------------
-START="$(date +%s)"
-cadaver < cadaver.script			#cp $TEST_FILE_PATH $WEB_DAV_MOUNT_POINT
-END="$(date +%s)"
-ELAPSED="$(expr $END - $START)"
-SIZE= ls -la $TEST_FILE_PATH | awk '{print $5}'
-echo Elapsed time: $ELAPSED
-rm cadaver.script
+	START="$(date +%s)"
+	#$CMD                    #cadaver < cadaver.script       
 
-sleep 1
-kill $BWM_PID
-
-BWM_FILE_LO_PATH=$BASE_DIR/measures/$WEB_DAV_HOST/$WEB_DAV_PATH/bwm-up-lo.csv
-echo "Start time" > $BWM_FILE_LO_PATH
-echo $START >> $BWM_FILE_LO_PATH
-echo "End time" >> $BWM_FILE_LO_PATH
-echo $END >> $BWM_FILE_LO_PATH
-echo "Elapsed" >> $BWM_FILE_LO_PATH
-echo $ELAPSED >> $BWM_FILE_LO_PATH
-echo "Size" >> $BWM_FILE_LO_PATH
-echo $SIZE >>  $BWM_FILE_LO_PATH
-echo "unix_timestamp;iface_name;bytes_out;bytes_in;bytes_total;packets_out;packets_in;packets_total;errors_out;errors_in" >>  $BWM_FILE_LO_PATH
-sed '/total/d' $BWM_FILE_PATH >> $BWM_FILE_LO_PATH
-rm $BWM_FILE_PATH
+ftp -inv $HOST_NAME <<ENDFTP
+user $2 $3
+put $TEST_FILE_SERVER_PATH $SERVER_PATH
+bye
+ENDFTP
 
 
+
+       END="$(date +%s)"
+       ELAPSED="$(expr $END - $START)"
+       SIZE= ls -la $TEST_FILE_SERVER_PATH | awk '{print $5}'
+       echo Elapsed time: $ELAPSED
+       rm cadaver.script
+       sleep 1
+       kill $BWM_PID
+}
+
+
+function formatOutput {
+
+# ----------------------- Format output-------------------------
+        if [ "$1" = "up" ];
+        then
+                BWM_FILE_LO_SERVER_PATH=$BASE_DIR/measures/$HOST_NAME/$SERVER_PATH/bwm-up-lo.csv
+        fi
+
+        if [ "$1" = "down" ];
+        then
+                BWM_FILE_LO_SERVER_PATH=$BASE_DIR/measures/$HOST_NAME/$SERVER_PATH/bwm-down-lo.csv
+        fi
+        echo "Start time" > $BWM_FILE_LO_SERVER_PATH
+        echo $START >> $BWM_FILE_LO_SERVER_PATH
+        echo "End time" >> $BWM_FILE_LO_SERVER_PATH
+        echo $END >> $BWM_FILE_LO_SERVER_PATH
+        echo "Elapsed" >> $BWM_FILE_LO_SERVER_PATH
+        echo $ELAPSED >> $BWM_FILE_LO_SERVER_PATH
+        echo "Size" >> $BWM_FILE_LO_SERVER_PATH
+        echo $SIZE >>  $BWM_FILE_LO_SERVER_PATH
+        echo "unix_timestamp;iface_name;bytes_out;bytes_in;bytes_total;packets_out;packets_in;packets_total;errors_out;errors_in" >>  $BWM_FILE_LO_SERVER_PATH
+        sed '/total/d' $BWM_FILE_SERVER_PATH >> $BWM_FILE_LO_SERVER_PATH
+        rm $BWM_FILE_SERVER_PATH
+
+}
+
+
+
+#----------------FTP---------------------
+DIRECTION=up
+initVariables $1
+initMeasurePathAndFile
+initMeasureFileAndCadaverScript $DIRECTION $1 $2 $3
+start $1 $2 $3
+# formatOutput $DIRECTION
 
