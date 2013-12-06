bb2dcb27fa53d1eb5d67126cdc8062f58dfe3164
S. Koulouzis <sS.Koulouzis@uva.nl>
2013-12-06 17:03:15 +0000

    scripts for measuring uploads

---
+++ lobcder-tests/scripts/measureUploads.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+loopNum=2
+srcFilePath=$1
+iface=$2
+multithread=$3
+
+dest="http://user:secret@host:8080/lobcder/dav/"
+
+if [[ -d $srcFilePath ]]; then
+  size=`du -sb $srcFilePath | awk '{print $1}'`
+else
+  size=$(stat -c%s "$srcFilePath")
+fi
+fileSize=$(echo "($size * 8) / (1000 * 1000)" |bc -l)
+
+for ((i = 0 ; i < $loopNum ; i++)); do
+  if [ -n "$multithread" ]; then
+    ./uploadFile.sh  $srcFilePath $dest $iface mmm
+  else
+    ./uploadFile.sh  $srcFilePath $dest $iface
+  fi
+  mv report.csv report-$fileSize-$i.csv
+done
+
+header="fileSize(MBit);speed(MBit/sec)"
+echo $header > collectedUpload.csv
+sum=0;
+for f in `ls report-$fileSize-*.csv`
+do
+    sed -n 2p $f | awk -F "\"*;\"*" '{ print $7 ";" $8 }' >> collectedUpload.csv
+    sed -n 2p $f | awk -F "\"*;\"*" '{ print $8 }' >> speed
+done 
+
+
+stats=`R -q -e "x <- read.csv('speed', header = F); summary(x); sd(x[ , 1])"`
+#echo $stats
+#> x <- read.csv('speed', header = F); summary(x); sd(x[ , 1]) V1 Min. :50.16 1st Qu.:51.19 Median :52.23 Mean :52.23 3rd Qu.:53.27 Max. :54.30 [1] 2.933017 > 
+mean=`echo $stats | awk -F "(\"*:\"*)" '{ print $5}' | awk '{ print $1}'`
+stdev=`echo $stats | awk '{ print $26 }'`
+rm speed
+
+
+lineNum=`expr $loopNum + 1`
+echo "=AVERAGE(A2:A$lineNum);=AVERAGE(B2:B$lineNum)" >> collectedUpload.csv
+echo "=STDEV(A2:A$lineNum);=STDEV(B2:B$lineNum)" >> collectedUpload.csv
+#echo " ;$mean" >> collectedUpload.csv
+#echo " ;$stdev" >> collectedUpload.csv
+
+header="fileSize(MBit);MeanSpeed(MBit/sec);StdevSpeed(MBit/sec)"
+echo $header > stats.csv
+echo "$fileSize;$mean;$stdev" >> stats.csv
