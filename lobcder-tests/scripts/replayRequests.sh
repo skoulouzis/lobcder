2022f0b95f8646e3f4cc2d66c268ce46f41fd991
S. Koulouzis <sS.Koulouzis@uva.nl>
2014-05-18 14:28:06 +0000

    Dump curl out to /dev/null

--- lobcder-tests/scripts/replayRequests.sh
+++ lobcder-tests/scripts/replayRequests.sh
@@ -8,25 +8,33 @@ dbName=lobcderDB2
 lobUser=user
 lobPass=pass
 
-
-#  
+basePath=$HOME/Downloads/
+servers/apache-tomcat-6.0.36=CATALINA_HOME
 
 t0=`mysql -u$sqlUser -p$sqlPass -s -N -e "select unix_timestamp(timeStamp) from $dbName.requests_table limit 1"`
-mysql -u$sqlUser -p$sqlPass -s -N -e  "select methodName, requestURL, unix_timestamp(timeStamp)-$t0, userAgent, contentLen from $dbName.requests_table" > result.csv
+mysql -u$sqlUser -p$sqlPass -s -N -e  "select methodName, requestURL, unix_timestamp(timeStamp)-$t0, userAgent, contentLen from $dbName.requests_table" > /tmp/result.csv
 
 
 #prepere files 
 while IFS=$'\t' read method url timestamp userAgent contentLen
 do
-  if [ $method = 'PUT' ]; then
+  if [ $method = 'PUT' ]; then  
     xpath=${url%/*}
     path=`echo $xpath | sed "s/^http:\/\///g"`
-    echo "mkdir -p $path"
-    mkdir -p $path
     fileName=${url##*/}
-    dd if=/dev/urandom of=$path/$fileName bs=1 count=$contentLen
+    relativePath=${path:27}
+    absPath=$basePath/$relativePath
+    if [[ ! -f $absPath/$fileName ]]; then
+      echo "mkdir -p $path"
+      mkdir -p $path
+      echo "dd if=/dev/urandom of=/tmp/$path/$fileName bs=1 count=$contentLen"
+      dd if=/dev/urandom of=/tmp/$path/$fileName bs=1 count=$contentLen
+    fi
+    
   fi
-done < result.csv
+done < /tmp/result.csv
+
+numOfReq=`wc -l < /tmp/result.csv`
 
 
 tn=0
@@ -35,27 +43,48 @@ do
     xpath=${url%/*}
     path=`echo $xpath | sed "s/^http:\/\///g"`
     fileName=${url##*/}
+    relativePath=${path:27}
+    absPath=$basePath/$relativePath
+     if [ -e $absPath/$fileName ]; then
+      path=$absPath
+    else
+      path=/tmp/$path
+     fi
     case "$method" in
-    PUT) echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' -T $path/$fileName $url  > temp.html"
-	 curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' -T $path/$fileName $url  > temp.html
+    PUT)
+	echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' -T $path/$fileName $url  > /dev/null"
+	 curl -iks -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' -T $path/$fileName $url  > /dev/null
 	 ;;
-    LOCK) echo "curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent '$userAgent' $url"
-	 token=`curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent '$userAgent' $url  | grep "Lock-Token: <opaquelocktoken:" 2>&1`
-	 mkdir -P $path
-	 echo $token > $path/$fileName.token
+    LOCK)
+	echo "curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent '$userAgent' $url"
+	 token=`curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent \'"$userAgent"\' $url  | grep "Lock-Token: <opaquelocktoken:" 2>&1`
+	 mkdir -p /tmp/$path
+	 echo "Token:  $token" 
+	 echo "Token file: /tmp/$path/$fileName.token"
+	 echo $token > /tmp/$path/$fileName.token
 	 ;;
-    UNLOCK) token=`cat $path/$fileName.token`
+    UNLOCK)
+	token=`cat /tmp/$path/$fileName.token`
 	 echo "curl -iks -u $lobUser:$lobPass -X $method --header "$token" --user-agent '$userAgent' $url"
-	 curl -iks -u $lobUser:$lobPass -X $method --header "$token" --user-agent '$userAgent' $url
+	 curl -iks -u $lobUser:$lobPass -X $method --header "$token" --user-agent \'"$userAgent"\' $url
 	 ;;
-    *) 	echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' $url  > temp.html"
-	curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' $url > temp.html
+    *) echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' $url  > /dev/null"
+	curl -iks -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' $url > /dev/null
 	;;
     esac
     
     sleepTime=`expr $timestamp - $tn`
     tn=$timestamp
-    echo $sleepTime
+#     echo $sleepTime
     
-#     sleep $sleepTime
-done < result.csv
+#      sleep $sleepTime
+     counter=$(( $counter + 1 ))
+     tmp=$((100 *  $counter))
+     progress=$(($tmp / $numOfReq))
+     echo "Request: $counter / $numOfReq  Progress: $progress %"
+done < /tmp/result.csv
+
+
+
+# grep -c 'Prediction Result:' $CATALINA_HOME/logs/*.log && grep -c 'Prediction Result: Hit' $CATALINA_HOME/logs/juli.2014-05-13.log  &&  grep -c 'Prediction Result: Miss' $CATALINA_HOME/logs/juli.2014-05-13.$CATALINA_HOME/logs/*.log && grep -c 'Prediction Result: Non' $CATALINA_HOME/logs/juli.2014-05-13.log
+# grep -c 'Prediction Result:' $CATALINA_HOME/logs/*.log && grep -c 'Prediction Result: Hit' $CATALINA_HOME/logs/*.log && grep -c 'Prediction Result: Miss' $CATALINA_HOME/logs/*.log && grep -c 'Prediction Result: Non' log
