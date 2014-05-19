ca75c37cfccce746abf467ca1efd53a3cecc6f43
S. Koulouzis <sS.Koulouzis@uva.nl>
2014-05-19 15:01:40 +0000

    Implemented Markov predictor with DB connection

--- lobcder/src/main/java/nl/uva/cs/lobcder/predictors/MarkovPredictor.java
+++ lobcder/src/main/java/nl/uva/cs/lobcder/predictors/MarkovPredictor.java
@@ -35,9 +35,10 @@ import nl.uva.cs.lobcder.util.MyDataSource;
 @Log
 public class MarkovPredictor extends MyDataSource implements Predictor {
 
-    Graph graph = new Graph();
+    Graph graph = null;
 
-    public MarkovPredictor() throws NamingException {
+    public MarkovPredictor() throws NamingException, SQLException {
+        graph = new Graph();
     }
 
     @Override
@@ -69,40 +70,54 @@ public class MarkovPredictor extends MyDataSource implements Predictor {
 
     class Graph extends MyDataSource {
 
-        public Graph() throws NamingException {
+        public Graph() throws NamingException, SQLException {
+            deleteAll();
+        }
+
+        private void deleteAll() throws SQLException {
+            try (Connection connection = getConnection()) {
+                try (PreparedStatement ps = connection.prepareStatement("DELETE FROM successor_table")) {
+                    ps.executeUpdate();
+                }
+                try (PreparedStatement ps = connection.prepareStatement("DELETE FROM occurrences_table")) {
+                    ps.executeUpdate();
+                }
+                connection.commit();
+            }
         }
 
         void addTransision(LobState from, LobState to) throws SQLException {
-            double getWeight = -1;
+            double weight = -1;
             int uid = -1;
             try (Connection connection = getConnection()) {
                 try (PreparedStatement ps = connection.prepareStatement("select uid, weight from "
-                        + "successor_table where keyVal = ? ")) {
+                        + "successor_table where keyVal = ? and lobStateID = ?")) {
                     ps.setString(1, from.getID());
-                    ps.setString(1, to.getID());
+                    ps.setString(2, to.getID());
                     ResultSet rs = ps.executeQuery();
                     if (rs.next()) {
                         uid = rs.getInt(1);
-                        getWeight = rs.getDouble(2);
+                        weight = rs.getDouble(2);
                     }
                 }
-                if (getWeight > -1) {
-                    getWeight++;
+                if (weight > -1) {
+                    weight++;
                     try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            " UPDATE graph_table SET keyVal = ?, lobStateID = ? , weight = ? , WHERE uid = ?", Statement.RETURN_GENERATED_KEYS)) {
-                        preparedStatement.setInt(1, uid);
+                            "UPDATE successor_table SET weight = ? WHERE uid = ?", Statement.RETURN_GENERATED_KEYS)) {
+                        preparedStatement.setDouble(1, weight);
+                        preparedStatement.setInt(2, uid);
                         preparedStatement.executeUpdate();
                         ResultSet rs = preparedStatement.getGeneratedKeys();
                         rs.next();
                     }
                 } else {
-                    getWeight = 1;
+                    weight = 1;
                     try (PreparedStatement preparedStatement = connection.prepareStatement(
-                            "INSERT INTO graph_table(keyVal, lobStateID , weight)"
+                            "INSERT INTO successor_table(keyVal, lobStateID , weight)"
                             + " VALUES (?, ?, ? )", Statement.RETURN_GENERATED_KEYS)) {
                         preparedStatement.setString(1, from.getID());
-                        preparedStatement.setString(1, to.getID());
-                        preparedStatement.setDouble(3, getWeight);
+                        preparedStatement.setString(2, to.getID());
+                        preparedStatement.setDouble(3, weight);
                         preparedStatement.executeUpdate();
                         ResultSet rs = preparedStatement.getGeneratedKeys();
                         rs.next();
@@ -121,7 +136,13 @@ public class MarkovPredictor extends MyDataSource implements Predictor {
                     if (rs.next()) {
                         String nextID = rs.getString(1);
                         String[] methodNRes = nextID.split(",");
-                        nextState = new LobState(Request.Method.valueOf(methodNRes[0]), methodNRes[1]);
+                        String resource;
+                        if (methodNRes.length <= 1) {
+                            resource = "/";
+                        } else {
+                            resource = methodNRes[1];
+                        }
+                        nextState = new LobState(Request.Method.valueOf(methodNRes[0]), resource);
                     }
                 }
                 connection.commit();
