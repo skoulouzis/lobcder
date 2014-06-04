eea6dc820501393f32c48945ce62bf6b744100c1
S. Koulouzis <sS.Koulouzis@uva.nl>
2014-06-04 18:11:00 +0000

    Changed properties

--- lobcder/src/main/resources/initLOBDB.sql
+++ lobcder/src/main/resources/initLOBDB.sql
@@ -1,3 +1,3 @@
-create database lobcderDB8;
+create database lobcderDB2;
 GRANT ALL PRIVILEGES on lobcderDB2.* to lobcder@localhost IDENTIFIED by 'RoomC3156';
 GRANT SUPER ON *.* to lobcder@localhost IDENTIFIED by 'RoomC3156';
