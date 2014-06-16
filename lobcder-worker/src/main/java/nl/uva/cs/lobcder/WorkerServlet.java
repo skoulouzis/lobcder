b698cdfa0f2d2b0bb6002a17f2f8b72654be8bb4
S. Koulouzis <sS.Koulouzis@uva.nl>
2014-06-16 13:09:37 +0000

    Tweak drop line conditions. Use percentage

--- lobcder-worker/src/main/java/nl/uva/cs/lobcder/WorkerServlet.java
+++ lobcder-worker/src/main/java/nl/uva/cs/lobcder/WorkerServlet.java
@@ -547,11 +547,12 @@ public final class WorkerServlet extends HttpServlet {
         int read;
         long total = 0;
         double speed;
-        double rateOfChange, change, prevChange = 0;
+        double rateOfChange = 0;
         double speedPrev = 0;
         long startTime = System.currentTimeMillis();
         int count = 0;
         String d = "";
+        double maxSpeed = -1;
         double thresshold = 100.0 * Math.exp(coefficient * (size / (1024.0 * 1024.0)));
         while ((read = in.read(buffer)) > 0) {
             output.write(buffer, 0, read);
@@ -560,19 +561,20 @@ public final class WorkerServlet extends HttpServlet {
             if (progress >= thresshold && Math.round(progress) % progressThresshold == 0) {
                 long elapsed = System.currentTimeMillis() - startTime;
                 speed = (total / elapsed);
-                change = (100.0 * speed) / speedPrev;
+                if (speed >= maxSpeed) {
+                    maxSpeed = speed;
+                }
+                rateOfChange = (100.0 * maxSpeed) / speedPrev;
                 speedPrev = speed;
-                prevChange = change;
-                rateOfChange = change - prevChange;
                 d += "progressThresshold: " + thresshold + " speed: " + speed + " rateOfChange: " + rateOfChange + " speedPrev: " + speedPrev + " progress: " + progress + "\n";
                 if (rateOfChange < lim) {
                     count++;
                     Logger.getLogger(WorkerServlet.class.getName()).log(Level.WARNING, "We will not tolarate this !!!! Next time line is off");
                     //This works with export ec=18; while [ $ec -eq 18 ]; do curl -O -C - -L --request GET -u user:pass http://localhost:8080/lobcder/dav/large_file; export ec=$?; done
-//                    if (count >= warnings) {
-                    Logger.getLogger(WorkerServlet.class.getName()).log(Level.WARNING, "We will not tolarate this !!!! Find a new worker. rateOfChange: {0}", rateOfChange);
-                    break;
-//                    }
+                    if (count >= warnings) {
+                        Logger.getLogger(WorkerServlet.class.getName()).log(Level.WARNING, "We will not tolarate this !!!! Find a new worker. rateOfChange: {0}", rateOfChange);
+                        break;
+                    }
                 }
             }
         }
