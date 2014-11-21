2ec12c23b0051e320a1294061d31c5b42db384e6
skoulouzis <skoulouzis@uva.nl>
2014-11-21 16:08:23 +0100

    Added speed logging and metadata reporting

--- lobcder-master/src/main/java/nl/uva/cs/lobcder/rest/Archive.java
+++ lobcder-master/src/main/java/nl/uva/cs/lobcder/rest/Archive.java
@@ -28,10 +28,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import nl.uva.cs.lobcder.rest.wrappers.Stats;
 
 /**
  *
@@ -47,27 +51,28 @@ public class Archive extends CatalogueHelper {
 
     @GET
     @Path("/getzip/{name:.+}")
-    public Response getZip(@PathParam("name") String path)  {
+    public Response getZip(@PathParam("name") String path) {
         @AllArgsConstructor
         class Folder {
+
             String path;
             LogicalData logicalData;
         }
 
         final String rootPath;
-        if(path.endsWith("/")) {
+        if (path.endsWith("/")) {
             rootPath = path.substring(0, path.length() - 1);
         } else {
             rootPath = path;
         }
         int index = rootPath.lastIndexOf('/');
         final String rootName;
-        if(index != -1) {
+        if (index != -1) {
             rootName = rootPath.substring(index + 1);
         } else {
             rootName = rootPath;
         }
-        if(rootName.isEmpty()){
+        if (rootName.isEmpty()) {
             throw new WebApplicationException(Response.Status.NOT_ACCEPTABLE);
         }
         final MyPrincipal principal = (MyPrincipal) request.getAttribute("myprincipal");
@@ -76,23 +81,28 @@ public class Archive extends CatalogueHelper {
         StreamingOutput result = new StreamingOutput() {
             @Override
             public void write(OutputStream out) throws IOException, WebApplicationException {
+
+
+                Stack<Folder> folders;
                 try (Connection connection = catalogue.getConnection()) {
                     LogicalData rootElement = catalogue.getLogicalDataByPath(io.milton.common.Path.path(rootPath), connection);
                     if (rootElement == null) {
-                        throw new WebApplicationException(404);
+                        throw new WebApplicationException(Response.Status.NOT_FOUND);
                     }
                     try (ZipOutputStream zip = new ZipOutputStream(out)) {
                         ZipEntry ze;
-                        Stack<Folder> folders = new Stack<>();
+                        folders = new Stack<>();
                         Permissions p = catalogue.getPermissions(rootElement.getUid(), rootElement.getOwner(), connection);
                         if (principal.canRead(p)) {
-                            if(rootElement.isFolder()) {
+                            if (rootElement.isFolder()) {
                                 folders.add(new Folder("/" + rootName, rootElement));
                             } else {
                                 ze = new ZipEntry("/" + rootName);
                                 zip.putNextEntry(ze);
-                                copyStream(catalogue.getPdriDescrByGroupId(rootElement.getPdriGroupId()), zip);
+                                List<PDRIDescr> pdris = catalogue.getPdriDescrByGroupId(rootElement.getPdriGroupId());
+                                copyStream(pdris, zip);
                                 zip.closeEntry();
+                                getCatalogue().addViewForRes(rootElement.getUid());
                             }
                         }
 
@@ -101,6 +111,7 @@ public class Archive extends CatalogueHelper {
                             ze = new ZipEntry(folder.path + "/");
                             ze.setTime(folder.logicalData.getModifiedDate());
                             zip.putNextEntry(ze);
+                            getCatalogue().addViewForRes(folder.logicalData.getUid());
                             for (LogicalData ld : catalogue.getChildrenByParentRef(folder.logicalData.getUid(), connection)) {
                                 Permissions entry_p = catalogue.getPermissions(ld.getUid(), ld.getOwner(), connection);
                                 if (principal.canRead(entry_p)) {
@@ -112,40 +123,68 @@ public class Archive extends CatalogueHelper {
                                         zip.putNextEntry(ze);
                                         copyStream(catalogue.getPdriDescrByGroupId(ld.getPdriGroupId()), zip);
                                         zip.closeEntry();
+                                        getCatalogue().addViewForRes(ld.getUid());
                                     }
                                 }
                             }
                         }
                     }
                 } catch (Exception e) {
-                    if(e instanceof WebApplicationException){
-                        throw (WebApplicationException)e;
+                    if (e instanceof WebApplicationException) {
+                        throw (WebApplicationException) e;
                     } else {
                         throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
                     }
                 }
+
             }
         };
-        Response.ResponseBuilder response = Response.ok(result, "application/zip" );
+
+
+
+
+
+        Response.ResponseBuilder response = Response.ok(result, "application/zip");
         return response.header("Content-Disposition", "attachment; filename=" + rootName + ".zip").build();
     }
 
     private void copyStream(List<PDRIDescr> pdriDescrList, OutputStream os) {
+        double start = System.currentTimeMillis();
+
         for (PDRIDescr pdriDescr : pdriDescrList) {
             try {
                 PDRI pdri = PDRIFactory.getFactory().createInstance(pdriDescr, false);
-                InputStream in = pdri.getData();
-                byte[] copyBuffer = new byte[Constants.BUF_SIZE];
-                int len;
-                while ((len = in.read(copyBuffer)) != -1) {
-                    os.write(copyBuffer, 0, len);
+                long total;
+                try (InputStream in = pdri.getData()) {
+                    byte[] copyBuffer = new byte[Constants.BUF_SIZE];
+                    int len;
+                    total = 0;
+                    while ((len = in.read(copyBuffer)) != -1) {
+                        os.write(copyBuffer, 0, len);
+                        total += len;
+                    }
                 }
-                in.close();
+
+
+                double elapsed = System.currentTimeMillis() - start;
+                double speed = ((total * 8.0) * 1000.0) / (elapsed * 1000.0);
+
+                Stats stats = new Stats();
+                stats.setSource(pdri.getHost());
+                stats.setDestination(request.getRemoteAddr());
+                stats.setSpeed(speed);
+                stats.setSize(total);
+                String msg = "Source: " + stats.getSource() + " Destination: " + stats.getDestination() + " Tx_Speed: " + speed + " Kbites/sec Tx_Size: " + total + " bytes";
+                try {
+                    getCatalogue().setSpeed(stats);
+                } catch (SQLException ex) {
+                    Logger.getLogger(Archive.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                Archive.log.log(Level.INFO, msg);
                 return;
             } catch (Throwable th) {
-                th.printStackTrace();
+                Logger.getLogger(Archive.class.getName()).log(Level.SEVERE, null, th);
             }
         }
     }
-
 }
