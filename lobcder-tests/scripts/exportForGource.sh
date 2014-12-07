e48b2286ed39c4aa267af17bf28b4b30cd129bb1
skoulouzis <skoulouzis@uva.nl>
2014-12-07 16:04:26 +0100

    Added all events

--- lobcder-tests/scripts/exportForGource.sh
+++ lobcder-tests/scripts/exportForGource.sh
@@ -1,9 +1,11 @@
-sqlUser=root
-sqlPass=hondos
-dbName=imported
+sqlUser=user
+sqlPass=password
+dbName=DB
 CATALINA_HOME=$HOME/apache-tomcat-6.0.36
 
 #mysql -u$sqlUser -p$sqlPass -s -N -e  "UPDATE $dbName.requests_table SET userAgent = 'unknown' WHERE userAgent = '' OR userAgent is NULL"
+
+
 mysql -u$sqlUser -p$sqlPass -s -N -e  "SELECT UNIX_TIMESTAMP(timeStamp), userName, 
 
 
@@ -14,6 +16,13 @@ when 'MKCOL' then 'A'
 when 'LOCK' then 'M'
 when 'UNLOCK' then 'M'
 when 'PROPPATCH' then 'M'
+when 'PROPFIND' then 'M'
+when 'OPTIONS' then 'M'
+when 'GET' then 'M'
+when 'HEAD' then 'M'
+when 'TRACE' then 'M'
+when 'CONNECT' then 'M'
+when 'COPY' then 'M'
 when 'MOVE' then 'M'
 when 'DELETE' then 'D'
 END AS AMD,
@@ -33,12 +42,20 @@ requestURL NOT LIKE '%lobcder/dav/' and
   methodName = 'POST' or
   methodName = 'MKCOL' or 
   --  (M)odified
+  methodName = 'LOCK' or 
+  methodName = 'UNLOCK' or 
   methodName = 'PROPPATCH' or
   methodName = 'MOVE' or
+  methodName = 'PROPFIND' or
+  methodName = 'COPY' or
+  methodName = 'OPTIONS' or
+  methodName = 'GET' or
+  methodName = 'HEAD' or
+  methodName = 'TRACE' or
+  methodName = 'CONNECT' or  
   -- (D)eleted. 
   methodName = 'DELETE')
 ORDER BY timeStamp;" > /tmp/result.csv
-
 
 # while IFS=$'\t' read timestamp userName AMD requestURL
 # do
@@ -54,6 +71,8 @@ sed -i 's/http\:\/\/149.156.10.138:8080\/lobcder\///g' /tmp/result.csv
 sed -i 's/http\:\/\/149.156.10.138\/lobcder\///g' /tmp/result.csv
 sed -i 's/https\:\/\/149.156.10.138:8443\/lobcder\///g' /tmp/result.csv
 sed -i 's/https\:\/\/149.156.10.138:443\/lobcder\///g' /tmp/result.csv
+sed -i 's/https\:\/\/149.156.10.138\/lobcder\///g' /tmp/result.csv
+sed -i 's/http\:\/\/149.156.10.138:8081\/lobcder\///g' /tmp/result.csv
 sed -i 's/http\:\/\/lobcder.tk\/lobcder\///g' /tmp/result.csv
 sed -i 's/http\:\/\/lobcder.tk:8080\/lobcder\///g' /tmp/result.csv
 sed -i 's/https\:\/\/lobcder.tk:8443\/lobcder\///g' /tmp/result.csv
@@ -62,23 +81,44 @@ sed -i 's/https\:\/\/lobcder.vph.cyfronet.pl\/lobcder\///g' /tmp/result.csv
 sed -i 's/http\:\/\/lobcder.vph.cyfronet.pl\/lobcder\///g' /tmp/result.csv
 sed -i 's/http:\/\/lobcder.vph.cyfronet.pl:8080\/lobcder\///g' /tmp/result.csv
 sed -i 's/https:\/\/lobcder.vph.cyfronet.pl:99\/lobcder\///g' /tmp/result.csv
+sed -i 's/http:\/\/lobcder.vph.cyfronet.pl:90\/lobcder\///g' /tmp/result.csv
 sed -i 's/https:\/\/lobcder.vph.cyfronet.pl:8443\/lobcder\///g' /tmp/result.csv
 sed -i 's/https:\/\/lobcder.vph.cyfronet.pl:443\/lobcder\///g' /tmp/result.csv
 sed -i 's/https\:\/\/localhost:8443\/lobcder\///g' /tmp/result.csv
 sed -i 's/https\:\/\/localhost:443\/lobcder\///g' /tmp/result.csv
 sed -i 's/http\:\/\/localhost:8080\/lobcder\///g' /tmp/result.csv
+sed -i 's/http\:\/\/127.0.0.1\/lobcder\///g' /tmp/result.csv
+sed -i 's/http\:\/\/127.0.0.1:8080\/lobcder\///g' /tmp/result.csv
+sed -i 's/https\:\/\/127.0.0.1\/lobcder\///g' /tmp/result.csv
 sed -i 's/ZGVtbzE6ZDNtb1Bhc3N3ZA==/demo1/g' /tmp/result.csv
+sed -i 's/http\:\/\/server.cyfronet.pl:8080\/lobcder\///g' /tmp/result.csv
+sed -i 's/http\:\/\/elab.lab.uvalight.net\/lobcder\///g' /tmp/result.csv
 
 
 sed -i 's/ZGVtbzE6ZDNtb1Bhc3N3ZA==/demo1/g' /tmp/result.csv
 
 
 sed -i "s/Jakarta Commons-HttpClient*/Jakarta/g" /tmp/result.csv
+sed -i "s/Jakarta\ Commons-HttpClient\/[0-9]./Jakarta/g" /tmp/result.csv
+
+
 sed -i "s/cadaver*/cadaver/g" /tmp/result.csv
+sed -i "s|cadaver\/[0-9].[0-9][0-9].[0-9]\ neon\/[0-9].[0-9][0-9].[0-9]|cadaver|g" /tmp/result.csv
+
 sed -i "s/python*/python/g" /tmp/result.csv
+sed -i "s|python-requests\/[0-9].[0-9].[0-9]\ Cpython\/[0-9].[0-9].[0-9]\ Linux\/[0-9].[0-9].[0-9][0-9]-[0-9][0-9][0-9]-[a-z][a-z][0-9]|python|g" /tmp/result.csv
+sed -i "s|python-requests\/[0-9].[0-9].[0-9]\ Cpython\/[0-9].[0-9].[0-9]\ Linux\/[0-9].[0-9][0-9].[0-9]-xxxx-std-ipv6-64|python|g"  /tmp/result.csv
+sed -i "s|python-requests\/[0-9].[0-9].[0-9]\ Cpython\/[0-9].[0-9].[0-9]\ Linux\/[0-9].[0-9][0-9].[0-9]-xxxx-grs-ipv6-64|python|g"  /tmp/result.csv
+sed -i "s|python-requests\/[0-9].[0-9].[0-9]\ Cpython\/[0-9].[0-9].[0-9] Windows\/[0-9]|python|g" /tmp/result.csv
+
 sed -i "s/Python*/python/g" /tmp/result.csv
 sed -i "s/davfs2*/davfs2/g" /tmp/result.csv
+sed -i "s|davfs2\/[0-9].[0-9].[0-9]\ neon\/[0-9].[0-9].|davfs2|g"  /tmp/result.csv
 sed -i "s/Mozilla*/Mozilla/g" /tmp/result.csv
+
+sed -i "s#Mozilla\/[0-9].[0-9]\ (Windows\ NT\ [0-9].[0-9])\ AppleWebKit\/[0-9][0-9][0-9].[0-9][0-9]\ (KHTML,\ like\ Gecko)\ Chrome\/[0-9][0-9].[0-9].[0-9][0-9][0-9].[0-9][0-9][0-9]\ Safari/[0-9]37.36#Safari#g" /tmp/result.csv
+
+
 sed -i "s/curl*/curl/g" /tmp/result.csv
 sed -i "s/gvfs*/gvfs/g" /tmp/result.csv
 sed -i "s/BitKinez*/BitKinez/g" /tmp/result.csv
@@ -93,6 +133,7 @@ sed -i "s/IPv4Scan*/IPv4Scan/g" /tmp/result.csv
 sed -i "s/Benchmark*/Benchmark/g" /tmp/result.csv
 
 
+sed -i '/1395853038\tjuanarenas\tD\tdav\//d' /tmp/result.csv
 sed -i 's/\t/|/g' /tmp/result.csv
 
 
