7df73e99ee657192fd0eec5de79db3f2d1f3b9be
skoulouzis <skoulouzis@uva.nl>
2015-03-31 20:57:49 +0200

    create report.csv for replay script

--- lobcder-tests/scripts/replayRequests.sh
+++ lobcder-tests/scripts/replayRequests.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
 
 
-
 sqlUser=root
-sqlPass=pass
-dbName=lobcderDB2
-lobUser=user
-lobPass=pass
+sqlPass=rootPass
+dbName=dbName
+lobUser=admin
+lobPass=admin
 
 basePath=$HOME/Downloads/
-servers/apache-tomcat-6.0.36=CATALINA_HOME
+CATALINA_HOME=servers/apache-tomcat-6.0.36
 
 t0=`mysql -u$sqlUser -p$sqlPass -s -N -e "select unix_timestamp(timeStamp) from $dbName.requests_table limit 1"`
 mysql -u$sqlUser -p$sqlPass -s -N -e  "select methodName, requestURL, unix_timestamp(timeStamp)-$t0, userAgent, contentLen from $dbName.requests_table" > /tmp/result.csv
 
 
+numOfReq=`wc -l < /tmp/result.csv`
 #prepere files 
 while IFS=$'\t' read method url timestamp userAgent contentLen
 do
-  if [ $method = 'PUT' ]; then  
+#   if [ $method = 'GET' ] || [ $method = 'PUT' ] ; then  
+  if [ $method = 'PUT' ] ; then  
     xpath=${url%/*}
     path=`echo $xpath | sed "s/^http:\/\///g"`
     fileName=${url##*/}
     relativePath=${path:27}
     absPath=$basePath/$relativePath
     if [[ ! -f $absPath/$fileName ]]; then
-      echo "mkdir -p $path"
-      mkdir -p $path
-      echo "dd if=/dev/urandom of=/tmp/$path/$fileName bs=1 count=$contentLen"
-      dd if=/dev/urandom of=/tmp/$path/$fileName bs=1 count=$contentLen
+      echo "mkdir -p $absPath"
+      mkdir -p $absPath
+      echo "dd if=/dev/urandom of=$absPath/$fileName bs=1 count=$(($contentLen/10))"
+      dd if=/dev/urandom of=$absPath/$fileName bs=1 count=$(($contentLen/10))
     fi
-    
   fi
+     counter=$(( $counter + 1 ))
+     tmp=$((100 *  $counter))
+     progress=$(($tmp/$numOfReq))
+     echo "File: $counter / $numOfReq  Progress: $progress %"
 done < /tmp/result.csv
 
-numOfReq=`wc -l < /tmp/result.csv`
+
 
 
 tn=0
+counter=0
+echo "Method,ContentLen,Elapsed" > report.csv
 while IFS=$'\t' read method url timestamp userAgent contentLen
 do
+    start=$(date +%s%N)
     xpath=${url%/*}
     path=`echo $xpath | sed "s/^http:\/\///g"`
     fileName=${url##*/}
@@ -52,12 +59,12 @@ do
      fi
     case "$method" in
     PUT)
-	echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' -T $path/$fileName $url  > /dev/null"
-	 curl -iks -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' -T $path/$fileName $url  > /dev/null
+	echo "curl -iks -L -u $lobUser:$lobPass -X $method --user-agent '$userAgent' -T $path/$fileName $url  > /dev/null"
+	 curl -iks -L -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' -T $path/$fileName $url  > /dev/null
 	 ;;
     LOCK)
-	echo "curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent '$userAgent' $url"
-	 token=`curl -iks -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent \'"$userAgent"\' $url  | grep "Lock-Token: <opaquelocktoken:" 2>&1`
+	echo "curl -iks -L -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent '$userAgent' $url"
+	 token=`curl -iks -L -u $lobUser:$lobPass -X $method --data @lock.xml --user-agent \'"$userAgent"\' $url  | grep "Lock-Token: <opaquelocktoken:" 2>&1`
 	 mkdir -p /tmp/$path
 	 echo "Token:  $token" 
 	 echo "Token file: /tmp/$path/$fileName.token"
@@ -65,14 +72,13 @@ do
 	 ;;
     UNLOCK)
 	token=`cat /tmp/$path/$fileName.token`
-	 echo "curl -iks -u $lobUser:$lobPass -X $method --header "$token" --user-agent '$userAgent' $url"
-	 curl -iks -u $lobUser:$lobPass -X $method --header "$token" --user-agent \'"$userAgent"\' $url
+	 echo "curl -iks -L -u $lobUser:$lobPass -X $method --header "$token" --user-agent '$userAgent' $url"
+	 curl -iks -L -u $lobUser:$lobPass -X $method --header "$token" --user-agent \'"$userAgent"\' $url
 	 ;;
-    *) echo "curl -iks -u $lobUser:$lobPass -X $method --user-agent '$userAgent' $url  > /dev/null"
-	curl -iks -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' $url > /dev/null
+    *) echo "curl -iks -L -u $lobUser:$lobPass -X $method --user-agent '$userAgent' $url  > /dev/null"
+	curl -iks -L -u $lobUser:$lobPass -X $method --user-agent \'"$userAgent"\' $url > /dev/null
 	;;
     esac
-    
     sleepTime=`expr $timestamp - $tn`
     tn=$timestamp
 #     echo $sleepTime
@@ -82,6 +88,10 @@ do
      tmp=$((100 *  $counter))
      progress=$(($tmp / $numOfReq))
      echo "Request: $counter / $numOfReq  Progress: $progress %"
+     
+     elasped=$((($(date +%s%N) - $start)/1000000))     
+     echo "$method,$contentLen,$elasped" >> report.csv
+     
 done < /tmp/result.csv
 
 
