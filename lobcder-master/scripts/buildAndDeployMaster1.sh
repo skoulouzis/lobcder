fb7f302602510b0c73fe3744a2223f97013b6005
skoulouzis <skoulouzis@uva.nl>
2015-04-09 19:11:57 +0300

    Script for deploying master in exogeni VMs

---
+++ lobcder-master/scripts/buildAndDeployMaster1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+apt-get upgrade
+
+export DEBIAN_FRONTEND=noninteractive
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+apt-get -q -y --force-yes  install mysql-server mysql-client oracle-java7-installer git maven2 erlang wget makepasswd nmap bwm-ng
+
+cd 
+
+
+wget http://apache.mirror.triple-it.nl/tomcat/tomcat-6/v6.0.43/bin/apache-tomcat-6.0.43.tar.gz
+tar -xavf apache-tomcat-6.0.43.tar.gz
+
+wget https://raw.githubusercontent.com/skoulouzis/lobcder/dev/lobcder-master/scripts/deployMaster.sh
+chmod +x deployMaster.sh
+wget https://raw.githubusercontent.com/skoulouzis/lobcder/dev/lobcder-master/scripts/storageFile
+./deployMaster.sh -n lobcderDB -p pass -u root -f storageFile -l lobcderAdmin -s lobcderAdmin -c ./apache-tomcat-6.0.43
