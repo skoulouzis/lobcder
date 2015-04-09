84a714c542653b75c8763ba43b4cef74b11ac1e1
skoulouzis <skoulouzis@uva.nl>
2015-04-09 18:33:47 +0300

    Fix comments  error

--- lobcder-master/src/main/resources/init.sql
+++ lobcder-master/src/main/resources/init.sql
@@ -148,14 +148,14 @@ CREATE TABLE requests_table (
 
 CREATE TABLE successor_table (
  uid SERIAL PRIMARY KEY,
- keyVal VARCHAR(1024), --INDEX(keyVal),
+ keyVal VARCHAR(1024), -- INDEX(keyVal),
  lobStateID VARCHAR(1024),
  weight DOUBLE
 ) ENGINE=InnoDB;
 
 CREATE TABLE occurrences_table (
  uid SERIAL PRIMARY KEY,
- keyVal VARCHAR(1024), --INDEX(keyVal),
+ keyVal VARCHAR(1024), -- INDEX(keyVal),
  occurrences BIGINT
 ) ENGINE=InnoDB;
 
@@ -196,8 +196,8 @@ CREATE TABLE features_table (
 
 CREATE TABLE speed_table (
   id SERIAL PRIMARY KEY,
-  src VARCHAR(1024),--INDEX(src),
-  dst VARCHAR(1024),--INDEX(dst),
+  src VARCHAR(1024),-- INDEX(src),
+  dst VARCHAR(1024),-- INDEX(dst),
   fSize ENUM('S', 'M','L','XL'), INDEX(fSize),
   averageSpeed DOUBLE,
   minSpeed DOUBLE,
