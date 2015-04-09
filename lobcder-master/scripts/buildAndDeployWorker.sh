94d0572122440e56f4839969c8bbcd69f11524d2
skoulouzis <skoulouzis@uva.nl>
2015-04-09 19:15:55 +0300

    Script for deploying worker in exogeni VMs

---
+++ lobcder-master/scripts/buildAndDeployWorker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+
+export DEBIAN_FRONTEND=noninteractive
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -q -y --force-yes  install oracle-java7-installer git maven2 erlang wget makepasswd nmap openvpn
+
+password="pass"
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd w -m -p $pass
+
+
+wget http://elab.lab.uvalight.net/~lobcder/apache-tomcat-6.0.36.tar.gz
+tar -xzvf apache-tomcat-6.0.36.tar.gz
+
+
+
+keytool -genkey -alias tomcat -keyalg RSA 
+
+
+
+./apache-tomcat-6.0.36/bin/shutdown.sh
+git clone https://github.com/skoulouzis/lobcder.git
+cd lobcder/lobcder-worker/
+mvn install 
+rm -r target/lobcder-worker
+mv target/lobcder-worker-?.?-SNAPSHOT target/lobcder-worker
+cd 
+cp -r lobcder/lobcder-worker/target/lobcder-worker apache-tomcat-6.0.36/webapps/
+./apache-tomcat-6.0.36/bin/startup.sh
+
+
+sleep 70 
+for i in {1..6}; do ping -c 1 192.168.100.$i; done
+
+
+for i in {1..7}; do wget http://localhost:8080/lobcder-worker/2/1; ./apache-tomcat-6.0.36/bin/shutdown.sh; ./apache-tomcat-6.0.36/bin/startup.sh;  done
+
+
+
