e1e012585e0f457405c1e80103358391143a1891
skoulouzis <skoulouzis@uva.nl>
2015-05-02 19:17:03 +0200

    Script to update pdri with storage backend

--- lobcder-master/scripts/manuallyReplicateToNewBackend.sh
+++ lobcder-master/scripts/manuallyReplicateToNewBackend.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-sqlUser=root
-sqlPass=pass
-dbName=DB
-replicaLocation=PATH
+replicaLocation=/home/alogo/Downloads/STORAGE/LOBCDER-REPLICA-SANDBOX
 
 ls $replicaLocation > fileNamesToReplcate
 
-#echo "CREATE TEMPORARY TABLE tmptable_1 SELECT * FROM pdri_table where" > tmpQuery
-echo "SELECT * FROM pdri_table where" > tmpQuery
+echo "CREATE TEMPORARY TABLE tmptable_1 SELECT * FROM $dbName.pdri_table where" > tmpQuery
+#echo "SELECT * FROM pdri_table where" > tmpQuery
 
 while read p; do
   echo fileName = \"$p\" >> tmpQuery
@@ -19,5 +16,8 @@ head -n -1 tmpQuery > query
 echo ";" >> query
 rm tmpQuery 
 
-mysql -u$sqlUser -p$sqlPass -s -N $dbName < query
-#mysql -u$sqlUser -p$sqlPass -s -N -e  "select methodName, requestURL, unix_timestamp(timeStamp)-$t0, userAgent, contentLen from $dbName.requests_table" > /tmp/result.csv
+#mysql -h localhost  -u$sqlUser -p$sqlPass -s lobcderDB2 < query
+#mysql -u$sqlUser -p$sqlPass -s -N -e "ALTER TABLE $dbName.tmptable_1 drop pdriId;"
+#mysql -u$sqlUser -p$sqlPass -s -N -e  $dbName "UPDATE $dbName.tmptable_1 SET storageSiteRef = $storageSiteID;"
+#mysql -u$sqlUser -p$sqlPass -s -N -e $dbName "INSERT INTO $dbName.pdri_table SELECT 0,$dbName.tmptable_1.* FROM $dbName.tmptable_1;"
+#mysql -u$sqlUser -p$sqlPass -s -N -e  $dbName "DROP TEMPORARY TABLE IF EXISTS $dbName.tmptable_1;"
