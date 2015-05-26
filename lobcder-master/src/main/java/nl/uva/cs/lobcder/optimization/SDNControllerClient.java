c9e902dda2530cf733cfd54c0b89b9a6dd75996b
skoulouzis <skoulouzis@uva.nl>
2015-05-26 23:39:00 +0200

    Added arp flow. Connect switches

--- lobcder-master/src/main/java/nl/uva/cs/lobcder/optimization/SDNControllerClient.java
+++ lobcder-master/src/main/java/nl/uva/cs/lobcder/optimization/SDNControllerClient.java
@@ -87,7 +87,7 @@ public class SDNControllerClient {
             }
         }
 
-        dest = "192.168.100.1";
+//        dest = "192.168.100.1";
 //        Logger.getLogger(SDNControllerClient.class.getName()).log(Level.INFO, "Destination: {0}", new Object[]{dest});
         if (!graph.containsVertex(dest)) {
             graph.addVertex(dest);
